14c069ec9a249b563f5213e8f44bb8274dad129d
Roi Meshulam <rohimesh21@gmail.com>
2023-06-13 14:55:26 +0300

    RecentEvents and Nasa

---
+++ src/Components/Cards/Sun/Sun.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { sunInfo } from './SunInfo'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import {Typography,Box} from '@mui/material'
+
+const Sun = () => {
+
+
+  return (
+    <div>Sun</div>
+  )
+}
+
+export default Sun
